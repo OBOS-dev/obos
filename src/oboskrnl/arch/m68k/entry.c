0e601631ff06d4abccf591e93d73f0982d891e41
oberrow <omarkberrow@gmail.com>
2024-08-06 09:08:27 -0400

    Fix build system bug.

--- src/oboskrnl/arch/m68k/entry.c
+++ src/oboskrnl/arch/m68k/entry.c
@@ -17,11 +17,6 @@ allocator_info* OBOS_KernelAllocator;
 process *OBOS_KernelProcess;
 timer_frequency CoreS_TimerFrequency;
 
-// DO NOT DELETE
-
-OBOS_PAGEABLE_VARIABLE int Arch_PageableDataVar;
-OBOS_PAGEABLE_RO_VARIABLE const int Arch_PageableRodataVar;
-
 void Arch_KernelEntryBootstrap()
 {
     // We can't really do anything yet.
