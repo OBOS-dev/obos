7c783b7d706ba49f6dd0b816ef97c9ad36fc32a5
oberrow <omarkberrow@gmail.com>
2024-08-08 13:59:06 -0400

    Remove changes from goldfish_rtc.h

--- src/oboskrnl/arch/m68k/goldfish_rtc.h
+++ src/oboskrnl/arch/m68k/goldfish_rtc.h
@@ -10,12 +10,3 @@
 
 #include <arch/m68k/interrupt_frame.h>
 
-void Arch_PICClearPending();
-void Arch_PICDisable(uint8_t line);
-void Arch_PICEnable(uint8_t line);
-uint8_t Arch_PICGetPendingCount();
-uint32_t Arch_PICGetPending();
-void Arch_PICRegisterIRQ(uint8_t line, uint8_t irq);
-void Arch_PICMaskIRQ(uint8_t line, bool mask);
-void Arch_PICHandleIRQ(interrupt_frame*);
-void Arch_PICHandleSpurious(interrupt_frame*);
