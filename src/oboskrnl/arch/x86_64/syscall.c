6d402eb427df80d362fd7b8f26a154ffa9e61726
Omar Berrow <omarkberrow@gmail.com>
2025-03-15 17:25:55 -0400

    misc: Add syscalls to syscall to name table.

--- src/oboskrnl/arch/x86_64/syscall.c
+++ src/oboskrnl/arch/x86_64/syscall.c
@@ -186,6 +186,9 @@ const char* syscall_to_string[] = {
     "Sys_FdOpenAtEx",
     "Sys_Mkdir",
     "Sys_MkdirAt",
+    "Sys_Chdir",
+    "Sys_ChdirEnt",
+    "Sys_GetCWD",
 };
 
 void Arch_LogSyscall(uintptr_t rdi, uintptr_t rsi, uintptr_t rdx, uintptr_t r8, uintptr_t r9, uint32_t eax)
