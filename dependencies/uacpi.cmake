cde9981bbba175a39f2658000e730f7ddb8b7d92
Omar Berrow <omarkberrow@gmail.com>
2025-03-22 16:01:48 -0400

    misc: Update uACPI

--- dependencies/uacpi.cmake
+++ dependencies/uacpi.cmake
@@ -9,7 +9,7 @@ if (OBOS_REFRESH_DEPENDENCIES)
 		# In honor of the old uACPI link
 		#GIT_REPOSITORY https://github.com/UltraOS/uACPI.git
 		GIT_REPOSITORY https://github.com/uACPI/uACPI.git
-		GIT_TAG 2.0.0
+		GIT_TAG 2.1.0
 		SOURCE_DIR ${CMAKE_SOURCE_DIR}/dependencies/uACPI
 	)
 	FetchContent_MakeAvailable(uACPI)
