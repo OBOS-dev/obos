0493162d4d1562b75167a573c5cd68e94342ca82
Omar Berrow <omarkberrow@gmail.com>
2025-03-25 22:04:10 -0400

    vfs: Add struct irp.

---
+++ src/oboskrnl/vfs/irp.h
@@ -0,0 +1,46 @@
+/*
+ * oboskrnl/vfs/irp.h
+ *
+ * Copyright (c) 2025 Omar Berrow
+*/
+
+#pragma once
+
+#include <int.h>
+#include <error.h>
+
+#include <vfs/limits.h>
+#include <vfs/vnode.h>
+
+#include <driver_interface/header.h>
+
+#include <locks/event.h>
+
+enum irp_op {
+    IRP_READ,
+    IRP_WRITE,
+};
+typedef struct irp {
+    // Set when the operation is complete.
+    // EVENT_NOTIFICATION.
+    event evnt;
+    union {
+        void *buff;
+        const void* cbuff;
+    };
+    size_t blkCount;
+    uoff_t blkOffset;
+    union {
+        size_t nBlkRead;
+        size_t nBlkWritten;
+    };
+    dev_desc desc;
+    obos_status status;
+    // If dryOp is true, then no bytes should be read/written, but
+    // evnt should still be set when blkCount bytes can be read/written.
+    bool dryOp : 1;
+    enum irp_op op : 1;
+} irp;
+// desc can be nullptr if request->desc can be implied from vn.
+obos_status VfsH_IRPSubmit(vnode* vn, irp* request, const dev_desc* desc);
+obos_status VfsH_IRPWait(irp* request);
