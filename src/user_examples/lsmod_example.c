a40fddde5dca39e89350802f8da8e9099783bcce
Omar Berrow <omarkberrow@gmail.com>
2025-09-19 09:57:28 -0400

    misc: Add lsmod command

---
+++ src/user_examples/lsmod_example.c
@@ -0,0 +1,47 @@
+#include <obos/syscall.h>
+#include <obos/error.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char** argv)
+{
+    if (argc < 2)
+    {
+        handle curr = HANDLE_INVALID;
+        do {
+            char name[64] = {};
+            size_t name_size = 64;
+            curr = syscall1(Sys_EnumerateLoadedDrivers, curr);
+            obos_status status = syscall3(Sys_QueryDriverName, curr, name, &name_size);
+            if (obos_is_error(status))
+            {
+                if (curr == HANDLE_INVALID)
+                {
+                    name_size = sizeof("End of list");
+                    memcpy(name, "End of list", name_size);
+                }
+                else
+                {
+                    fprintf(stderr, "Sys_QueryDriverName: %d\n", status);
+                    return -1;
+                }
+            }
+            printf("%.*s\n", (int)name_size, name);
+            if (curr != HANDLE_INVALID)
+                syscall1(Sys_HandleClose, curr);
+        } while(curr != HANDLE_INVALID);
+    }
+    else
+    {
+        const char* target_name = argv[1];
+        handle hnd = syscall1(Sys_FindDriverByName, target_name);
+        if (hnd != HANDLE_INVALID)
+        {
+            printf("%s\n", target_name);
+            syscall1(Sys_HandleClose, hnd);
+        }
+        else
+            return 2;
+    }
+    return 0;
+}
