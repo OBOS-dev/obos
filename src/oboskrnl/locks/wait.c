ed1abf4b4726deb85c337b0f0cc96183ad3fef84
Omar Berrow <omarkberrow@gmail.com>
2025-11-02 07:25:39 -0500

    core: Fix compilation error in release mode

--- src/oboskrnl/locks/wait.c
+++ src/oboskrnl/locks/wait.c
@@ -21,12 +21,18 @@
 
 #include <stdarg.h>
 
+#if OBOS_DEBUG
+#   define assert_initialized() OBOS_ASSERT(obj->initialized == true)
+#else
+#   define assert_initialized() (true)
+#endif
+
 obos_status Core_WaitOnObject(struct waitable_header* obj)
 {
     if (!obj)
         return OBOS_STATUS_INVALID_ARGUMENT;
     OBOS_ASSERT(Core_GetIrql() <= IRQL_DISPATCH);
-    OBOS_ASSERT(obj->initialized == true);
+    assert_initialized();
     if (Core_GetIrql() > IRQL_DISPATCH)
         return OBOS_STATUS_INVALID_IRQL;
     irql oldIrql = Core_RaiseIrql(IRQL_DISPATCH);
@@ -108,7 +114,7 @@ obos_status Core_WaitOnObjects(size_t nObjects, struct waitable_header** objs, s
     for (size_t i = 0; i < nObjects; i++)
     {
         struct waitable_header* const obj = objs[i];
-        OBOS_ASSERT(obj->initialized == true);
+        assert_initialized();
         irql spinlockIrql = Core_SpinlockAcquire(&obj->lock);
         if (obj->signaled)
         {
@@ -172,7 +178,7 @@ obos_status CoreH_SignalWaitingThreads(struct waitable_header* obj, bool all, bo
 {
     if (!obj)
         return OBOS_STATUS_INVALID_ARGUMENT;
-    OBOS_ASSERT(obj->initialized == true);
+    assert_initialized();
 
     OBOS_ASSERT(Core_GetIrql() <= IRQL_DISPATCH);
     if (Core_GetIrql() > IRQL_DISPATCH)
@@ -210,7 +216,7 @@ obos_status CoreH_AbortWaitingThreads(struct waitable_header* obj)
 {
     if (!obj)
         return OBOS_STATUS_INVALID_ARGUMENT;
-    OBOS_ASSERT(obj->initialized == true);
+    assert_initialized();
     obj->interrupted = 1;
     return CoreH_SignalWaitingThreads(obj, true, false);
 }
@@ -219,7 +225,7 @@ void CoreH_ClearSignaledState(struct waitable_header* obj)
 {
     if (!obj || !obj->use_signaled)
         return;
-    OBOS_ASSERT(obj->initialized == true);
+    assert_initialized();
     irql oldIrql = Core_SpinlockAcquire(&obj->lock);
     obj->signaled = false;
     Core_SpinlockRelease(&obj->lock, oldIrql);
