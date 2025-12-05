ee0a0c95db01503a9af1af1975a808902db50519
Omar Berrow <omarkberrow@gmail.com>
2025-12-04 20:25:03 -0500

    misc: fix generate-initrd scripts to create /sys/perm

--- scripts/generate_initrd-m68k.sh
+++ scripts/generate_initrd-m68k.sh
@@ -1,15 +1,8 @@
 export old_pwd=$PWD
 cd $(git rev-parse --show-toplevel)
-if [[ ! -d tar ]]
-then
-    mkdir tar
-fi
+mkdir -p tar tar/dev tar/sys/perm
 # TODO: Drivers?
 cp out/init tar/
-if [[ ! -d tar/dev ]]
-then
-    mkdir tar/dev
-fi
 cd tar
 tar -H ustar -cf ../config/initrd.tar `ls -A`
 cd $old_pwd
--- scripts/generate_initrd-x86_64.sh
+++ scripts/generate_initrd-x86_64.sh
@@ -1,9 +1,8 @@
 export old_pwd=$PWD
 cd $(git rev-parse --show-toplevel)
-if [[ ! -d tar ]]
-then
-    mkdir tar
-fi
+mkdir -p tar
+mkdir -p tar/dev
+mkdir -p tar/sys/perm
 cp out/uart tar
 cp out/ahci tar
 cp out/extfs tar
@@ -13,10 +12,6 @@ cp out/i8042 tar
 cp out/libps2 tar
 cp out/e1000 tar
 cp out/init tar
-if [[ ! -d tar/dev ]]
-then
-    mkdir tar/dev
-fi
 cd tar
 tar -H ustar -cf ../config/initrd.tar `ls -A`
 cd $old_pwd
