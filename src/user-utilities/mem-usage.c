d6fbb67f09ab4f772ef01c1bcadea4124dce9417
Omar Berrow <omarkberrow@gmail.com>
2025-12-28 23:00:30 -0500

    user-utilities: remove reserved memory count from mem-usage

--- src/user-utilities/mem-usage.c
+++ src/user-utilities/mem-usage.c
@@ -13,8 +13,7 @@ typedef struct memstat
     size_t pageable;
     // The size of all non-pageable memory (memory that cannot be paged out).
     size_t nonPaged;
-    // The size of all uncommitted (reserved) memory. (memory allocated with VMA_FLAGS_RESERVE that has not yet been committed).
-    size_t reserved;
+    size_t resv;
     // The amount of total page faults on this context.
     size_t pageFaultCount;
     // The amount of soft page faults on this context.
@@ -53,23 +52,21 @@ int main()
     char unit = 'B';
     size_t divisor = 0;
     get_div_and_unit(pmem, &unit, &divisor);
-    printf("Physical memory usage: %ld%c\n", pmem/divisor, unit);
+    printf("Physical memory usage: %f%c\n", pmem/(float)divisor, unit);
     if (cmem != OBOS_STATUS_UNIMPLEMENTED)
     {
         get_div_and_unit(cmem, &unit, &divisor);
-        printf("Cached Memory: %ld%c\n", cmem/divisor, unit);
+        printf("Cached Memory: %f%c\n", cmem/(float)divisor, unit);
     }
     memstat global_memory_usage = {};
     syscall2(Sys_ContextGetStat, HANDLE_INVALID, &global_memory_usage);
     get_div_and_unit(global_memory_usage.committedMemory, &unit, &divisor);
-    printf("Total committed memory: %ld%c\n", global_memory_usage.committedMemory/divisor, unit);
+    printf("Total committed memory: %f%c\n", global_memory_usage.committedMemory/(float)divisor, unit);
     get_div_and_unit(global_memory_usage.paged, &unit, &divisor);
-    printf("Total paged memory: %ld%c\n", global_memory_usage.paged/divisor, unit);
+    printf("Total paged memory: %f%c\n", global_memory_usage.paged/(float)divisor, unit);
     get_div_and_unit(global_memory_usage.pageable, &unit, &divisor);
-    printf("Total pageable memory: %ld%c\n", global_memory_usage.pageable/divisor, unit);
+    printf("Total pageable memory: %f%c\n", global_memory_usage.pageable/(float)divisor, unit);
     get_div_and_unit(global_memory_usage.nonPaged, &unit, &divisor);
-    printf("Total non paged memory: %ld%c\n", global_memory_usage.nonPaged/divisor, unit);
-    get_div_and_unit(global_memory_usage.reserved, &unit, &divisor);
-    printf("Total reserved memory: %ld%c\n", global_memory_usage.reserved/divisor, unit);
+    printf("Total non paged memory: %f%c\n", global_memory_usage.nonPaged/(float)divisor, unit);
     return 0;
 }
