8036f5a51e893159e0efef96cbdea28ca3c6c802
Omar Berrow <omarkberrow@gmail.com>
2026-01-17 17:19:47 -0500

    misc: export shared ptr utils

--- src/oboskrnl/utils/shared_ptr.h
+++ src/oboskrnl/utils/shared_ptr.h
@@ -25,13 +25,13 @@ typedef struct shared_ptr
     void(*onRef)(struct shared_ptr*);
 } shared_ptr;
 
-shared_ptr* OBOS_SharedPtrConstructSz(shared_ptr* ptr, void* obj, size_t sz);
+OBOS_EXPORT shared_ptr* OBOS_SharedPtrConstructSz(shared_ptr* ptr, void* obj, size_t sz);
 #define OBOS_SharedPtrConstruct(ptr, obj_ptr) ({ typeof(obj_ptr) _a = (obj_ptr); OBOS_SharedPtrConstructSz((ptr), _a, sizeof(*_a)); })
-void OBOS_SharedPtrRef(shared_ptr* ptr);
+OBOS_EXPORT void OBOS_SharedPtrRef(shared_ptr* ptr);
 #define OBOS_SharedPtrCopy(ptr) ({OBOS_SharedPtrRef((ptr)); ptr; })
-void OBOS_SharedPtrUnref(shared_ptr* ptr);
+OBOS_EXPORT void OBOS_SharedPtrUnref(shared_ptr* ptr);
 // udata is the struct allocator_info* used to allocate the object.
 // if udata is nullptr, OBOS_KernelAllocator is assumed
-void OBOS_SharedPtrDefaultFree(void* udata, shared_ptr* ptr);
-void OBOS_SharedPtrAssertRefs(shared_ptr* ptr);
+OBOS_EXPORT void OBOS_SharedPtrDefaultFree(void* udata, shared_ptr* ptr);
+OBOS_EXPORT void OBOS_SharedPtrAssertRefs(shared_ptr* ptr);
 #define OBOS_SharedPtrGet(type, ptr_) ({ typeof(ptr_) _a = (ptr_); OBOS_SharedPtrAssertRefs(_a); (type*)(_a->obj); })
