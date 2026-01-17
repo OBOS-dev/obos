79ef83a95561379f0887dccf19c9f0019f86491d
Omar Berrow <omarkberrow@gmail.com>
2026-01-17 16:38:30 -0500

    usb: implement device configuration

--- src/oboskrnl/driver_interface/usb.c
+++ src/oboskrnl/driver_interface/usb.c
@@ -10,6 +10,7 @@
 #include <memmanip.h>
 
 #include <mm/sglist.h>
+#include <mm/context.h>
 #include <mm/pmm.h>
 
 #include <driver_interface/usb.h>
@@ -95,6 +96,160 @@ obos_status Drv_USBPortAttached(usb_controller* ctlr, const usb_device_info* inf
     return OBOS_STATUS_SUCCESS;
 }
 
+static obos_status get_descriptor(usb_dev_desc* desc, uint8_t type, uint8_t idx, uint8_t length, void* buff)
+{
+    usb_irp_payload payload = {};
+
+    obos_status status = DrvH_ScatterGather(&Mm_KernelContext, buff, length, &payload.payload.setup.regions, &payload.payload.setup.nRegions, 61, true);
+    if (obos_is_error(status))
+        return status;
+
+    payload.trb_type = USB_TRB_CONTROL;
+    payload.endpoint = 0;
+    payload.payload.setup.bmRequestType = 0x80;
+    payload.payload.setup.bRequest = USB_GET_DESCRIPTOR;
+    payload.payload.setup.wValue = ((uint16_t)type << 8) | idx;
+    payload.payload.setup.wLength = length;
+
+    irp* req = nullptr;
+    status = Drv_USBIRPSubmit2(desc, (void**)&req, &payload, true);
+    if (obos_is_error(status))
+    {
+        DrvH_FreeScatterGatherList(&Mm_KernelContext, buff, length, payload.payload.setup.regions, payload.payload.setup.nRegions);
+        VfsH_IRPUnref(req);
+        return status;
+    }
+
+    status = Drv_USBIRPWait(desc, req);
+    
+    DrvH_FreeScatterGatherList(&Mm_KernelContext, buff, length, payload.payload.setup.regions, payload.payload.setup.nRegions);
+    VfsH_IRPUnref(req);
+    
+    return status;
+}
+
+struct interface {
+    usb_interface_descriptor* descriptor;
+    usb_endpoint_descriptor** endpoints;
+    size_t endpoint_count;
+};
+
+static obos_status configure_endpoint(usb_dev_desc* desc, usb_endpoint_descriptor* endpoint, bool dc)
+{
+    usb_irp_payload conf_ep = {};
+    conf_ep.trb_type = USB_TRB_CONFIGURE_ENDPOINT;
+    conf_ep.payload.configure_endpoint.deconfigure = dc;
+    if (!dc)
+    {
+        conf_ep.payload.configure_endpoint.endpoint_type = endpoint->bmAttributes & 0b11;
+        // TODO(oberrow): Maximum burst size
+        conf_ep.payload.configure_endpoint.max_burst_size = 0;
+        conf_ep.payload.configure_endpoint.max_packet_size = endpoint->wMaxPacketSize & 2047;
+    }
+    conf_ep.endpoint = endpoint->bEndpointAddress & 0xf;
+    bool dir = (endpoint->bEndpointAddress & BIT(7));
+
+    irp* req = nullptr;
+    obos_status status = Drv_USBIRPSubmit2(desc, (void**)&req, &conf_ep, dir);
+    if (obos_is_error(status))
+        return status;
+
+    status = Drv_USBIRPWait(desc, req);
+    VfsH_IRPUnref(req);
+    return status;
+}
+
+static obos_status configure_interface_eps(usb_dev_desc* desc, struct interface* iface)
+{
+    for (size_t i = 0; i < iface->endpoint_count; i++)
+    {
+        obos_status status = configure_endpoint(desc, iface->endpoints[i], false);
+        if (obos_is_error(status))
+        {
+            for (size_t j = 0; j < i; j++)
+                configure_endpoint(desc, iface->endpoints[j], true);
+            return status;
+        }
+        continue;
+    }
+    if (!desc->info.hid.class)
+    {
+        desc->info.hid.class = iface->descriptor->bInterfaceClass;
+        desc->info.hid.subclass = iface->descriptor->bInterfaceSubclass;
+        desc->info.hid.protocol = iface->descriptor->bInterfaceProtocol;
+    }
+    return OBOS_STATUS_SUCCESS;
+}
+
+static obos_status try_configuration(usb_dev_desc* ddesc, usb_configuration_descriptor* conf_desc, void* top)
+{
+    struct interface* interfaces = nullptr;
+    size_t nInterfaces = 0;
+    struct interface* current_interface = nullptr;
+
+    usb_descriptor_header* curr = usb_next_descriptor(conf_desc);
+    while (((uintptr_t)curr) < ((uintptr_t)top))
+    {
+        switch (curr->bDescriptorType) {
+            case USB_DESCRIPTOR_TYPE_INTERFACE:
+            {
+                interfaces = Reallocate(OBOS_KernelAllocator, interfaces, (nInterfaces+1)*sizeof(*interfaces), nInterfaces*sizeof(*interfaces), nullptr);
+                current_interface = &interfaces[nInterfaces++];
+                current_interface->descriptor = (void*)curr;
+                current_interface->endpoint_count = 0;
+                current_interface->endpoints = nullptr;
+                break;
+            }
+            case USB_DESCRIPTOR_TYPE_ENDPOINT:
+            {
+                if (!current_interface)
+                    break;
+                size_t new_size = (current_interface->endpoint_count+1) * sizeof(usb_endpoint_descriptor*);
+                size_t old_size = current_interface->endpoint_count * sizeof(usb_endpoint_descriptor*);
+                current_interface->endpoints = Reallocate(OBOS_KernelAllocator, current_interface->endpoints, new_size, old_size, nullptr);
+                usb_endpoint_descriptor** ep = &current_interface->endpoints[current_interface->endpoint_count++];
+                (*ep) = (void*)curr;
+                break;
+            }
+            default: break;
+        }
+        
+        curr = usb_next_descriptor(curr);
+    }
+
+    obos_status status = OBOS_STATUS_SUCCESS;
+
+    // We have a map of the interface and endpoint descriptors,
+    // now try to initialize the endpoints.
+
+    usb_irp_payload set_configuration = {};
+    set_configuration.trb_type = USB_TRB_CONTROL;
+    set_configuration.payload.setup.nRegions = 0;
+    set_configuration.payload.setup.bmRequestType = 0x00;
+    set_configuration.payload.setup.bRequest = USB_SET_CONFIGURATION;
+    set_configuration.payload.setup.wValue = conf_desc->bConfigurationValue;
+    set_configuration.payload.setup.wIndex = 0;
+    set_configuration.payload.setup.wLength = 0;
+
+    irp* req = nullptr;
+    status = Drv_USBIRPSubmit2(ddesc, (void**)&req, &set_configuration, false);
+    status = Drv_USBIRPWait(ddesc, req);
+    VfsH_IRPUnref(req);
+    if (obos_is_error(status))
+        return status;
+    
+    for (size_t i = 0; i < nInterfaces; i++)
+    {
+        status = configure_interface_eps(ddesc, &interfaces[i]);
+        Free(OBOS_KernelAllocator, interfaces[i].endpoints, interfaces[i].endpoint_count * sizeof(interfaces[i].endpoints[0]));
+        if (obos_is_success(status))
+            break;
+    }
+    Free(OBOS_KernelAllocator, interfaces, nInterfaces*sizeof(*interfaces));
+
+    return status;
+}
+
 OBOS_EXPORT obos_status Drv_USBPortPostAttached(usb_controller* ctlr, usb_dev_desc* desc)
 {
     OBOS_ENSURE(Core_GetIrql() < IRQL_DISPATCH);
@@ -102,7 +257,43 @@ OBOS_EXPORT obos_status Drv_USBPortPostAttached(usb_controller* ctlr, usb_dev_de
     if (!ctlr || !desc)
         return OBOS_STATUS_INVALID_ARGUMENT;
 
-    return OBOS_STATUS_SUCCESS;
+    OBOS_ALIGN(32) usb_device_descriptor dev_desc = {};
+    OBOS_ENSURE((((uintptr_t)&dev_desc) & 0xf) == 0 && "unaligned pointer");
+    obos_status status = get_descriptor(desc, USB_DESCRIPTOR_TYPE_DEVICE, 0, sizeof(dev_desc), &dev_desc);
+    if (obos_is_error(status))
+        return status;
+
+    desc->info.hid.class = dev_desc.bDeviceClass;
+    desc->info.hid.subclass = dev_desc.bDeviceSubclass;
+    desc->info.hid.protocol = dev_desc.bDeviceProtocol;
+
+    for (uint8_t conf = 0; conf < dev_desc.bNumConfigurations; conf++)
+    {
+        OBOS_ALIGN(32) usb_configuration_descriptor pre_conf_desc = {};
+        OBOS_ENSURE((((uintptr_t)&pre_conf_desc) & 0xf) == 0 && "unaligned pointer");
+        status = get_descriptor(desc, USB_DESCRIPTOR_TYPE_CONFIGURATION, 0, sizeof(pre_conf_desc), &pre_conf_desc);
+        if (obos_is_error(status))
+            return status;
+        
+        usb_configuration_descriptor* conf_desc = Allocate(OBOS_KernelAllocator, pre_conf_desc.wTotalLength, nullptr);
+        void* top = (void*)((uintptr_t)conf_desc + pre_conf_desc.wTotalLength);
+        status = get_descriptor(desc, USB_DESCRIPTOR_TYPE_CONFIGURATION, 0, pre_conf_desc.wTotalLength, conf_desc);
+        if (obos_is_error(status))
+        {
+            Free(OBOS_KernelAllocator, conf_desc, pre_conf_desc.wTotalLength);
+            return status;
+        }
+
+        status = try_configuration(desc, conf_desc, top);
+        Free(OBOS_KernelAllocator, conf_desc, pre_conf_desc.wTotalLength);
+        if (obos_is_success(status))
+            break;
+    }
+
+    if (obos_is_success(status))
+        OBOS_Debug("usb: successfully configured slot %d. slot hid=%02x:%02x:%02x\n", desc->info.slot, desc->info.hid.class, desc->info.hid.subclass, desc->info.hid.protocol);
+
+    return status;
 }
 
 obos_status Drv_USBPortDetached(usb_controller* ctlr, usb_dev_desc* desc)
@@ -133,12 +324,16 @@ obos_status Drv_USBIRPSubmit(usb_dev_desc* desc, void* req_p)
     return desc->controller->hdr->ftable.submit_irp(req_p);
 }
 
-OBOS_EXPORT obos_status Drv_USBIRPSubmit2(usb_dev_desc* desc, void** reqo, const usb_irp_payload* payload)
+OBOS_EXPORT obos_status Drv_USBIRPSubmit2(usb_dev_desc* desc, void** reqo, const usb_irp_payload* payload, bool dir)
 {
     if (!desc || !desc->controller || !desc->controller->hdr || !reqo || !payload)
         return OBOS_STATUS_INVALID_ARGUMENT;
 
     irp* req = VfsH_IRPAllocate();
+    if (dir)
+        req->op = IRP_READ;
+    else
+        req->op = IRP_WRITE;
     req->blkCount = sizeof(*payload);
     req->buff = Allocate(OBOS_KernelAllocator, sizeof(*payload), nullptr);
     memcpy(req->buff, payload, sizeof(*payload));
